42d06a7986e68c8b37e185c5c5281e10ef96efa0
enollog199-crypto <enollog199@gmail.com>
2026-04-07 14:17:59 +0800

    Create db_user.py

---
+++ db_user.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("user.db")
+c = conn.cursor()
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+)
+""")
+
+conn.commit()
+conn.close()
